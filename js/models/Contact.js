e11fd5487165b8587a52c839a0a7738b67e6e41b
sergio-nunez <sergio.nunez@laposte.net>
2024-07-26 17:59:22 +0200

    Feat(Contact):create contact class

---
+++ js/models/Contact.js
@@ -0,0 +1,15 @@
+/**
+ * Contact représente un contact disposant d'un :
+ * - nom
+ * - prenom
+ * - telephone
+ */
+
+export class Contact {
+
+    constructor(nom, prenom, telephone) {
+        this.nom = nom;
+        this.prednom = prenom;
+        this.telephone = telephone;
+    }
+}
