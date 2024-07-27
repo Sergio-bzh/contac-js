c53685d1d111757047134164500102af5b3a652c
sergio-nunez <sergio.nunez@laposte.net>
2024-07-27 10:15:19 +0200

    Feat(Contact): supprimerContact function updated

--- js/controller/contactController.js
+++ js/controller/contactController.js
@@ -4,18 +4,18 @@ let listContacts = [];
 
 
 
-const contact = new Contact("dominique", "anne", "0789654234"); ///* Instanciation d'un contact */
-listContacts.push(contact); ///* Push du contact dans le tableau */
+const contact = new Contact("dominique", "anne", "0789654234"); ///* Instanciation d'un contact en appellant la classe */
+listContacts.push(contact); ///* Push du contact dans le tableau listContacts */
 
 /* START OK OK OK OK */
 /* # # # # # # # # AJOUTER # # # # # # # # */
 function ajouterContact(nom, prenom, telephone){
 
-    const monContact = new Contact(nom, prenom, telephone);
+    const monContact = new Contact(nom, prenom, telephone); ///* la variable monContact n'existe qu'à l'intérieur de la procédure */
 
-    listContacts.push(monContact);
+    listContacts.push(monContact);  ///* Je n'ai pas utilisé le mot return (procédure) */
 }
-ajouterContact("NUNEZ", "Sergio", "0102030405");
+ajouterContact("NUNEZ", "Sergio", "0102030405"); ///* Instanciation d'un contact en appellant la fonction ajouterContact() */
 ajouterContact("CHOULET", "Anne", "0102030405");
 ajouterContact("CARRASCO", "Dominique", "0102030405");
 // console.log(listContacts);
@@ -24,6 +24,7 @@ ajouterContact("CARRASCO", "Dominique", "0102030405");
 function afficherContacts(listContacts){
     for(let i=0; i<listContacts.length; i++){
         let contact = listContacts[i];
+         ///* Je détaille l'objet contact pour l'affichage */
         contact = {
             nom: listContacts[i].nom,
             prenom: listContacts[i].prenom,
@@ -35,18 +36,16 @@ function afficherContacts(listContacts){
 }
 afficherContacts(listContacts)
 
-
 /* # # # # # # # # SUPPRIMER # # # # # # # # */
-/*
 function supprimerContact(index){
 
     let nom = listContacts.splice(index, 1);
-    nom = nom.join("");
-
-    return "Le contact "+ nom + " a bien été supprimé, et il reste encore : " + listContacts;
+    console.log(nom[0])
+    return console.log("Le contact " +" avec l'indice " +index+" ("+nom[0].prenom+" "+nom[0].nom+ ") a bien été supprimé.");
 }
 supprimerContact(1);
-*/
+console.log(listContacts);
+
 
 /* # # # # # # # # A # # # # # # # # */
 /* END OK OK OK OK */
